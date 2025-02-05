e524d3dda7b3db68753f78f9d33078ece3c6df9e
Fageltomten <caslund2005@gmail.com>
2025-02-05 11:38:42 +0100

    Dialoguetypes

--- ThesisProject/Assets/TutorialProject/Scripts/NPCscript.cs
+++ ThesisProject/Assets/TutorialProject/Scripts/NPCscript.cs
@@ -1,12 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NPCscript : MonoBehaviour, IInteractable
 {
 
-    [SerializeField]SO_Dialogue dialogue;
+    public enum DialogueType
+    {
+        Looping,
+        Random, 
+        Clamp
+    }
+
+    [SerializeField] DialogueType dialogueType;
+
+    [SerializeField]SO_Dialogue[] dialogue;
+
+
+    Queue<SO_Dialogue> dialogueQueue = new Queue<SO_Dialogue>();
+    int numberOfDialoguesLeft = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < dialogue.Length; i++)
+        {
+            dialogueQueue.Enqueue(dialogue[i]);
+            numberOfDialoguesLeft++;
+        }
+        
+    }
 
     public void Interact()
     {
-        DialogueManager.Instance.Queue(dialogue);
+        DialogueManager.Instance.Queue(SelectDialogue());
     }
+
+    SO_Dialogue SelectDialogue()
+    {
+        SO_Dialogue selectedDialouge = dialogueQueue.ElementAt(0);
+
+        switch (dialogueType)
+        {
+            case DialogueType.Looping:
+                dialogueQueue.Enqueue(dialogueQueue.Dequeue());
+                break;
+            case DialogueType.Clamp:
+                numberOfDialoguesLeft--;
+                if (numberOfDialoguesLeft > 0)
+                {
+                    dialogueQueue.Dequeue();
+                }
+                break;
+            case DialogueType.Random:
+                int randomIndex = Random.Range(0, numberOfDialoguesLeft);
+                return dialogue[randomIndex];
+                break;
+        }
+
+        return selectedDialouge;
+
+    }
+
+    
 }
